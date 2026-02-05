5021e874ee08ae6962f1a11ae7a242aa5857cef5
dumma rajesh <dumma.rajeshdumma2004@gmail.com>
2026-02-05 21:03:20 +0530

    javaScript

---
+++ Frontend/javascript/JavaScriptFundamentals/Strings.js
@@ -0,0 +1,9 @@
+// strings
+
+let singleQuotes = 'Hello World';
+let doubleQuotes = "Hello World";
+let templateLiteral = `Hello World`;
+
+let message1 = "It's a beautiful day";
+let message2 = 'He said "Hello"';
+let message3 = `She said "It's amazing!"`;
